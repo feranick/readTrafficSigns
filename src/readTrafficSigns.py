881f54b368828916e7c4a3ce6c54eded96e04891
Nicola Ferralis <feranick@hotmail.com>
2023-02-17 16:34:35 -0500

    Update keras.models.load_model to keras.saving.load_model New starting from TF 2.12

--- src/readTrafficSigns.py
+++ src/readTrafficSigns.py
@@ -18,7 +18,8 @@ from PIL import Image
 import os, sys, configparser
 from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
-from keras.models import Sequential, load_model
+from keras.models import Sequential
+from keras.saving import load_model
 from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
 
 #***************************************************
